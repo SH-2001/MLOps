fe828aa179bd031af957927cd009eaff5f7148af
Sander <sander.hermans@student.ehb.be>
2025-05-20 13:44:58 +0300

    Edited

--- MLOps_2/terraform/s3_buckets.tf
+++ MLOps_2/terraform/s3_buckets.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   for_each = { for s3 in var.s3_buckets : s3.key => s3 }
-  source   = "modules/s3-bucket"
+  source   = "./modules/s3-bucket"
 
   bucket = join(var.delimiter, [each.value.key, var.environment])
   tags   = merge(try(each.value.tags, {}), { environment = var.environment })
